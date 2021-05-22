c2c3276e3365fe0842cc7a166b3b51c619b60c03
Thuan0707 <thuandmce140131@fpt.edu.vn>
2021-05-22 23:46:14 +0700

    a
    a

--- AAH_Group8/app/src/main/java/com/example/aah_group8/MainActivity.java
+++ AAH_Group8/app/src/main/java/com/example/aah_group8/MainActivity.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn=findViewById(R.id.button1);
-
+        btn = findViewById(R.id.button1);
+        btn.setOnClickListener(v -> CameraMethod());
+    }
+    public void CameraMethod(){
         Intent switchActivityIntent = new Intent(this, CameraActivity.class);
         startActivity(switchActivityIntent);
     }
