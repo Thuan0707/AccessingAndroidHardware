15e4fddb1cff78bc543bfd07e5cf7aa21c64fbb3
Khuong Nguyen <khuongkg1412@gmail.com>
2021-05-22 22:31:58 +0700

    done clone

--- AAH_Group8/app/src/main/java/com/example/aah_group8/KhuongActivity.java
+++ AAH_Group8/app/src/main/java/com/example/aah_group8/KhuongActivity.java
@@ -3,7 +3,9 @@ package com.example.aah_group8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/*Nguyen Van Khuong
+* CE140417
+*/
 public class KhuongActivity extends AppCompatActivity {
 
     @Override
--- AAH_Group8/app/src/main/java/com/example/aah_group8/Model/aa.java
+++
@@ -1,4 +0,0 @@
-package com.example.aah_group8.Model;
-
-public class aa {
-}
