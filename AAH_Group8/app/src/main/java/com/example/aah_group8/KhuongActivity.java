5c3a1d340a6039296edcc1b6f1c95293c9b84a57
Khuong Nguyen <khuongkg1412@gmail.com>
2021-05-22 21:27:10 +0700

    nani

---
+++ AAH_Group8/app/src/main/java/com/example/aah_group8/KhuongActivity.java
@@ -0,0 +1,11 @@
+package com.example.aah_group8;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KhuongActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
