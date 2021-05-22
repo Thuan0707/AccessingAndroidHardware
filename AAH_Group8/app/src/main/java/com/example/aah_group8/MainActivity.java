5d55ec210b383a2837949c3e21ef330d8a7216db
Thuan0707 <thuandmce140131@fpt.edu.vn>
2021-05-22 20:48:27 +0700

    a
    a

--- AAH_Group8/app/src/main/java/com/example/aah_group8/MainActivity.java
+++ AAH_Group8/app/src/main/java/com/example/aah_group8/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//thuan
+//thuanadfaf
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
