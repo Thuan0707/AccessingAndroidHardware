21f4f98966c4b62f692a2443e4467171c7dee1ff
Khuong Nguyen <khuongkg1412@gmail.com>
2021-05-24 00:37:56 +0700

    done code
    comment not yet

--- AAH_Group8/app/src/main/java/com/example/aah_group8/MainActivity.java
+++ AAH_Group8/app/src/main/java/com/example/aah_group8/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.aah_group8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-//thuanầ
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, KhuongActivity.class);
+        startActivity(intent);
     }
 }
