ea390fc679778bf113d7d287a8c707c9138d90be
liêm phan <liemptce140563@fpt.edu.vn>
2021-05-27 09:39:18 +0700

    add Code

--- AAH_Group8/app/src/main/java/com/example/aah_group8/WifiScanningActivity.java
+++ AAH_Group8/app/src/main/java/com/example/aah_group8/WifiScanningActivity.java
@@ -9,6 +9,7 @@ import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -47,15 +48,19 @@ public class WifiScanningActivity extends AppCompatActivity {
         }
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(adapter);
+        Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(myIntent);
         scanWifi();
     }
-
     private void scanWifi() {
         arrayList.clear();
         registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         wifiManager.startScan();
         Toast.makeText(this, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
+
+
     }
+
     BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -67,4 +72,5 @@ public class WifiScanningActivity extends AppCompatActivity {
             }
         }
     };
+
 }
